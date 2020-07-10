3999bc2879580be518d36c11f7525bd964cd5eb0
kajal <tiwarikajal1306@gmail.com>
2020-07-10 22:16:44 +0530

    REFACTORED To get list og rides from ride repository and return the invoice

--- src/main/java/com/cabInvoiceGenerator/InvoiceGenerator.java
+++ src/main/java/com/cabInvoiceGenerator/InvoiceGenerator.java
@@ -6,6 +6,14 @@ public class InvoiceGenerator {
     private static final int COST_PER_TIME = 1;
     private static final double MIN_FARE = 5;
 
+
+    private RideRepository rideRepository;
+
+    public InvoiceGenerator()
+    {
+        this.rideRepository=new RideRepository();
+    }
+
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MIN_COST_PER_KM + time * COST_PER_TIME;
         if (totalFare < MIN_FARE)
@@ -23,9 +31,11 @@ public class InvoiceGenerator {
 
     public void addRides(String userId, Ride[] rides) {
 
+        rideRepository.addRides(userId,rides);
+
     }
 
     public InvoiceSummary getInvoiceSummary(String userId) {
-        return null;
+        return this.calculateFare(rideRepository.getRides(userId));
     }
 }
