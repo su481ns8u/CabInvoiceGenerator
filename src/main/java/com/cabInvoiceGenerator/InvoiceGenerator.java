4ff7915f0178808ad009213c18145e4fc9ac94ab
kajal <tiwarikajal1306@gmail.com>
2020-07-10 18:28:49 +0530

    ADDED Calculated distance and time

---
+++ src/main/java/com/cabInvoiceGenerator/InvoiceGenerator.java
@@ -0,0 +1,8 @@
+package com.cabInvoiceGenerator;
+
+public class InvoiceGenerator {
+
+    public double calculateFare(double distance, int time) {
+        return 0.0;
+    }
+}
---
+++ src/main/java/com/cabInvoiceGenerator/Ride.java
@@ -0,0 +1,4 @@
+package com.cabInvoiceGenerator;
+
+public class Ride {
+}
