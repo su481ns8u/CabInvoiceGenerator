188648d970a69647867428556ac1ede77415c91c
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-07-10 18:49:05 +0530

    ADDED Method for calculating fare

--- src/main/java/com/cabInvoiceGenerator/InvoiceGenerator.java
+++ src/main/java/com/cabInvoiceGenerator/InvoiceGenerator.java
@@ -2,7 +2,10 @@ package com.cabInvoiceGenerator;
 
 public class InvoiceGenerator {
 
+    private static final double MIN_COST_PER_KM = 10;
+    private static final int COST_PER_TIME = 1;
+
     public double calculateFare(double distance, int time) {
-        return 0.0;
+        return distance * MIN_COST_PER_KM + time * COST_PER_TIME;
     }
 }
